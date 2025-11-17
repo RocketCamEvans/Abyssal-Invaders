783a467ee8e6fcc23c84d1802550af4c5a535016
RocketCamEvans <cevans@rocketsoftware.com>
2025-11-17 09:12:57 -0600

    Add casino (#8)

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -7,5 +7,6 @@ from .combat import CombatController
 from .generation import GenerationController
 from .scoring import ScoringController
 from .inventory import InventoryController
+from .blackjack import BlackjackController
 
-__all__ = ['MovementController', 'CombatController', 'GenerationController', 'ScoringController', 'InventoryController']
+__all__ = ['MovementController', 'CombatController', 'GenerationController', 'ScoringController', 'InventoryController', 'BlackjackController']
